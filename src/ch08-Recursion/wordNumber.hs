aea35e351c91cde22d72949034639e35c9b4bc99
Zhihong Cheng <chengzh2008@gmail.com>
2017-05-20 21:30:52 -0700

    refactor digitToWord by using Enum class

--- src/ch08-Recursion/wordNumber.hs
+++ src/ch08-Recursion/wordNumber.hs
@@ -2,17 +2,13 @@ module WordNumber where
 
 import Data.List (intersperse)
 
+data Digit = Zero | One | Two | Three | Four |
+  Five | Six | Seven | Eight | Nine deriving (Eq, Show, Enum)
+
 digitToWord :: Int -> String
-digitToWord 0 = "zero"
-digitToWord 1 = "one"
-digitToWord 2 = "two"
-digitToWord 3 = "three"
-digitToWord 4 = "four"
-digitToWord 5 = "five"
-digitToWord 6 = "six"
-digitToWord 7 = "seven"
-digitToWord 8 = "eight"
-digitToWord 9 = "nine"
+digitToWord n
+  | n < 0 || n > 10 = error "invalid digit"
+  | otherwise = show (toEnum n :: Digit)
 
 digits :: Int -> [Int]
 digits n
