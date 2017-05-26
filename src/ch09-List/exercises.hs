c0108b0d2624771403051333046bfe2b62cdbec2
Zhihong Cheng <chengzh2008@gmail.com>
2017-05-25 21:24:26 -0700

    work on ch09 exercise

--- src/ch09-List/exercises.hs
+++ src/ch09-List/exercises.hs
@@ -1,1 +1,18 @@
 module Exercise where
+
+import Data.Char
+
+getUpper :: String -> String
+getUpper = filter isUpper
+
+capitalize :: String -> String
+capitalize "" = ""
+capitalize (c:cs) = (toUpper c) : cs
+
+capitalize' :: String -> String
+capitalize' "" = ""
+capitalize' (c:cs) = toUpper c : capitalize' cs
+
+
+capFirst :: String -> Char
+capFirst = toUpper . head
