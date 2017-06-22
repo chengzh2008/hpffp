8474f274035b6ed881336ebc7e108ddbae7e729a
Zhihong Cheng <chengzh2008@gmail.com>
2017-06-21 22:07:00 -0700

    finish testing

--- src/ch14-Testing/exercise/test/Spec.hs
+++ src/ch14-Testing/exercise/test/Spec.hs
@@ -1,6 +1,7 @@
 import Test.Hspec
 import WordNumber (digitToWord, digits, wordNumber)
 import Test.QuickCheck
+import Data.List
 
 main :: IO ()
 main = do
@@ -25,6 +26,15 @@ main = do
         property $ prop_half
   quickCheck prop_half
   quickCheck plusAssociative
+  quickCheck multipleAssociative
+  quickCheck quotRemLaw
+  quickCheck divModLaw
+  -- not hold
+  quickCheck powerNotAssociate
+  quickCheck (reverseList :: [Integer] -> Bool)
+  -- idempotence
+  quickCheck (f1 :: [String] -> Bool)
+  quickCheck (f2 :: [Integer] -> Bool)
 
 
 half :: (Fractional a) => a -> a
@@ -34,5 +44,37 @@ prop_half :: Double -> Bool
 prop_half x = ((*2) . half $ x) == x
 
 --plusAssociative
-plusAssociative ::  Double -> Double -> Double -> Bool
+plusAssociative ::  Integer -> Integer -> Integer -> Bool
 plusAssociative x y z = x + (y + z) == (x + y) + z
+
+multipleAssociative :: Integer -> Integer -> Integer -> Bool
+multipleAssociative x y z = x * (y * z) == (x * y) * z
+
+quotRemLaw :: Integer -> Integer -> Bool
+quotRemLaw x y
+  | y /= 0 = (quot x y) * y + rem x y == x
+  | otherwise = True
+
+divModLaw :: Integer -> Integer -> Bool
+divModLaw x y
+  | y /= 0 = (div x y) * y + mod x y == x
+  | otherwise = True
+
+powerNotAssociate :: Integer -> Integer -> Integer -> Bool
+powerNotAssociate x y z = x ^ (y ^ z) == (x ^ y) ^ z
+
+-- twice reverse of list is itself
+reverseList :: Eq a => [a] -> Bool
+reverseList x = (reverse . reverse $ x) == id x
+
+-- $ property
+dollarSign :: Eq a => (a -> a) -> a -> Bool
+dollarSign f a = (f $ a) == f a
+
+
+-- idempotence
+twice f = f . f
+fourTimes = twice . twice
+
+f1 x = (sort x) == twice sort x
+f2 x = (sort x) == fourTimes sort x
