d7ca3311c87369d199439c93dc6f7bef787e3672
Zhihong Cheng <chengzh2008@gmail.com>
2017-07-07 22:04:53 -0700

    finish ch16 functor

--- src/ch16-Functor/chapterExercise.hs
+++ src/ch16-Functor/chapterExercise.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
+
+import Text.Show.Functions
+
 -- Quant
 data Quant a b = Finance | Desk a | Bloor b deriving (Eq, Show)
 
@@ -36,3 +39,41 @@ data Parappa f g a = DaWrappa (f a) (g a) deriving (Eq, Show)
 
 instance (Functor f, Functor g) => Functor (Parappa f g) where
   fmap h (DaWrappa fa ga) = DaWrappa (fmap h fa) (fmap h ga)
+
+-- functor IgnoreOne
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b) deriving (Eq, Show)
+
+instance Functor g => Functor (IgnoreOne f g a) where
+  fmap h (IgnoringSomething fa gb) = IgnoringSomething fa $ fmap h gb
+
+-- functor Notorious goat
+data Notorious g o a t = Notorious (g o) (g a) (g t) deriving (Eq, Show)
+
+instance Functor g => Functor (Notorious g o a) where
+  fmap f (Notorious go ga gt) = Notorious go ga $ fmap f gt
+
+-- functor List
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons a ls) = Cons (f a) $ fmap f ls
+
+-- functor Tree
+data GoatLord a =
+    NoGoat
+  | OneGoat a
+  | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a) deriving (Eq, Show)
+
+instance Functor GoatLord where
+  fmap _ NoGoat = NoGoat
+  fmap f (OneGoat a) = OneGoat $ f a
+  fmap f (MoreGoats ga1 ga2 ga3) = MoreGoats (fmap f ga1) (fmap f ga2) (fmap f ga3)
+
+-- TODO: functor of a Mixture
+data TalkToMe a = Halt | Print String a | Read {unRead::String -> a} deriving (Show)
+
+instance Functor TalkToMe where
+  fmap _ Halt = Halt
+  fmap f (Print s a) = Print s $ f a
+  fmap f (Read g) = Read (f . g)
