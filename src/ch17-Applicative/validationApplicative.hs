dcc0f507437d391bc21715dce9d8099e88a368b8
chengzh2008 <chengzh2008@gmail.com>
2017-07-19 15:51:40 -0700

    update ch17

--- src/ch17-Applicative/validationApplicative.hs
+++ src/ch17-Applicative/validationApplicative.hs
@@ -1,4 +1,4 @@
-module MoreApplicative where
+module ValidationApplicative where
 
 import Data.Monoid
 import Test.QuickCheck
@@ -6,6 +6,33 @@ import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 
 data Validation err a = Failure' err | Success' a deriving (Eq, Show)
+
+-- natural transformation between validation and Either datatype
+-- eitherToValid . validToEither == id
+validToEither :: Validation e a -> Either e a
+validToEither (Failure' err) = Left err
+validToEither (Success' a) = Right a
+
+eitherToValid :: Either e a -> Validation e a
+eitherToValid (Left err) = Failure' err
+eitherToValid (Right a) = Success' a
+
+
+data Errors = DividedByZero | StackOverflow | MooglesChewedWires deriving (Eq, Show)
+
+-- the expected behaviors of the Validation applicative
+
+-- success = Success' (+1) <*> Success' 1
+-- success == Success' 2
+
+-- failure = Success' (+1) <*> Failure' [StackOverflow]
+-- failure == Failure' [StackOverflow]
+
+-- failure' = Failure' [StackOverflow] <*> Success' (+1)
+-- failure' == Failure' [StackOverflow]
+
+-- failures = Failure' [MonglesChewedWires] <*> Failure' [StackOverflow]
+-- failures == Failure' [MooglesChewedWires, StackOverflow]
 
 instance Functor (Validation e) where
   fmap _ (Failure' err) = Failure' err
