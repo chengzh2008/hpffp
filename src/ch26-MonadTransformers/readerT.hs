aca98881397e3c19b06d9105a3c6f26ebeee1720
Zhihong Cheng <chengzh2008@gmail.com>
2017-08-28 22:02:53 -0700

    half way through transformer chapter

---
+++ src/ch26-MonadTransformers/readerT.hs
@@ -0,0 +1,18 @@
+newtype ReaderT r m a = ReaderT { runReaderT :: r -> m a }
+
+instance Functor m => Functor (ReaderT r m) where
+  -- fmap :: (a -> b) -> ReaderT r m a -> ReaderT r m b
+  fmap f (ReaderT rma) = ReaderT $ (fmap . fmap) f rma
+
+instance Applicative m => Applicative (ReaderT r m) where
+  pure a = ReaderT $ pure (pure a)
+  ReaderT rmfa <*> ReaderT rma = ReaderT $ (<*>) <$> rmfa <*> rma
+
+instance Monad m => Monad (ReaderT r m) where
+  return = pure
+  -- rma :: r -> m a
+  -- f :: a -> ReaderT (r -> m b)
+  ReaderT rma >>= f = ReaderT $ do
+    \r -> do
+      v <- rma r
+      runReaderT (f v) r
---
+++ src/ch26-MonadTransformers/stateT.hs
@@ -0,0 +1,37 @@
+import Data.Tuple
+
+newtype StateT s m a = StateT { runStateT :: s -> m (a, s) }
+
+instance Functor m => Functor (StateT s m) where
+  -- f :: a -> b
+  -- TODO: figure out simpler solution
+  fmap f (StateT smas) =
+    StateT $ \s -> let m = smas s   -- m (a, s)
+                       u = fmap swap m -- m (s, a)
+                       v = (fmap . fmap) f u -- m (s, b)
+                   in fmap swap v -- m (b, s)
+
+instance (Monad m) => Applicative (StateT s m) where
+  pure a = StateT $ \s -> return (a, s)
+  -- smfa :: s -> m ((a -> b), s)
+  -- sma :: s -> m (a, s)
+  -- final :: s -> m (b, s)
+  -- here it will generate two state s' and s'', thus type constraint for m should be Monad, not applicative.
+  -- TODO: figure out simpler solution
+  StateT smfa <*> StateT sma =
+    StateT $ \s -> do
+      (f, s') <- smfa s -- m ((a -> b) , s)
+      (a , s'') <- sma s' -- m (a, s)
+      return (f a, s'') -- m (b, s)
+
+instance Monad m => Monad (StateT s m) where
+  return = pure
+  -- sma :: s -> m (a, s)
+  -- f :: a -> StateT  (s -> m (b, s))
+  -- final :: StateT (s -> m (b, s))
+  -- TODO: figure out simpler solution
+  StateT sma >>= f = StateT $ do
+    \s -> do
+      (a, s') <- sma s
+      (b, s'') <- runStateT (f a) s'
+      return (b, s'')
